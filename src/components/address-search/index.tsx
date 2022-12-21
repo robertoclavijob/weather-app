2bcfc2a14a2cc3bfab74aaeba14951f781aa379d
Roberto <claviro85@gmail.com>
2022-12-21 09:23:39 -0400

    Refactor code to select a station

--- src/components/address-search/index.tsx
+++ src/components/address-search/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getCoordsByAddress } from "../../services/geocoding-service";
 import { getWeeklyForecast } from "../../services/weather-service";
 
@@ -8,12 +8,18 @@ interface AddressSearchProps{
 }
 
 function AddressSearch(props: AddressSearchProps) {
-  const [address, setAddress] = useState('');
+  const [stationIndex, setStationIndex] = useState<number>();
 
-  const searchAddress = async (searchTerm: React.ChangeEvent<HTMLSelectElement>) => {
-    let station = props.stations[Number(searchTerm.target.value)];
+  const onSelectStation = async (selectedStation: React.ChangeEvent<HTMLSelectElement>) => {
+    let index = Number(selectedStation.target.value);
+    selectStationByIndex(index);
+  };
+
+  const selectStationByIndex = (index: number)=>{
+
+    let station = props.stations[index];
     let [long, lat] = station.geometry.coordinates;
-    setAddress(searchTerm.target.value);
+    setStationIndex(index);
     props.onUpdateForecast(lat, long);
 
     //TODO The next code calls the geocoding api (geocoding.geo.census.gov), but it is causing CORS issues
@@ -25,15 +31,15 @@ function AddressSearch(props: AddressSearchProps) {
     //   let { x, y } = firstMatch.coordinates;
     //   props.onUpdateForecast(x,y);
     // }
-  };
+  }
 
   return (
     <>
       <h1>Select a Weather Station</h1>
       <select
         placeholder="Search for an address"
-        onChange={searchAddress}
-        value={address}
+        onChange={onSelectStation}
+        value={stationIndex}
       >
         {props.stations.map((stationItem, index) => (
           <option key={index} value={index}>
