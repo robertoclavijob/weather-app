6e2e74904351f16a93e756227a0dcf7324f2a656
Roberto <claviro85@gmail.com>
2022-12-20 21:14:19 -0400

    Display error message if request to weather api failed

--- src/pages/weekly-forecast/index.tsx
+++ src/pages/weekly-forecast/index.tsx
@@ -1,3 +1,4 @@
+import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import AddressSearch from "../../components/address-search";
 import DayNightForecast from "../../components/day-night-forecast";
@@ -9,6 +10,7 @@ import "./WeeklyForecast.css";
 function WeeklyForecast() {
   const [forecast, setForecast] = useState<DayNightForecastDto[]>([]);
   const [stations, setStations] = useState<any[]>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     updateStations();
@@ -19,41 +21,50 @@ function WeeklyForecast() {
     setStations(stationsResult.data.features);
   }
   const updateForecast = async (lat: number, long: number) => {
-    const weatherResult = await getWeeklyForecast(lat, long);
-    let forecastByDay: DayNightForecastDto = {} as DayNightForecastDto;
+    setError('');
+    setForecast([]);
 
-    const forecastByDayList = [];
-    for (let forecastItem of weatherResult.data.properties.periods) {
-      const nameFirstWord = forecastItem.name.split(" ").shift();
-      if (!forecastByDay.name) {
-        if (forecastItem.number < 2) {
-          forecastByDay.name = DayEnum.TODAY;
-        } else {
-          forecastByDay.name = nameFirstWord;
+    try{
+      const weatherResult = await getWeeklyForecast(lat, long);
+      let forecastByDay: DayNightForecastDto = {} as DayNightForecastDto;
+
+      const forecastByDayList = [];
+      for (let forecastItem of weatherResult.data.properties.periods) {
+        const nameFirstWord = forecastItem.name.split(" ").shift();
+        if (!forecastByDay.name) {
+          if (forecastItem.number < 2) {
+            forecastByDay.name = DayEnum.TODAY;
+          } else {
+            forecastByDay.name = nameFirstWord;
+          }
+        }
+  
+        if (!forecastByDay.day) {
+          forecastByDay.day = forecastItem;
+          continue;
+        }
+  
+        if (!forecastByDay.night) {
+          forecastByDay.night = forecastItem;
+        }
+  
+        if (forecastByDay.name) {
+          forecastByDayList.push(forecastByDay);
+          forecastByDay = {} as DayNightForecastDto;
         }
       }
-
-      if (!forecastByDay.day) {
-        forecastByDay.day = forecastItem;
-        continue;
-      }
-
-      if (!forecastByDay.night) {
-        forecastByDay.night = forecastItem;
-      }
-
-      if (forecastByDay.name) {
-        forecastByDayList.push(forecastByDay);
-        forecastByDay = {} as DayNightForecastDto;
-      }
+      setForecast(forecastByDayList);
+    }catch(error: any){
+      console.log(error)
+      setError(error.message)
     }
-    setForecast(forecastByDayList);
   };
 
 
   return (
     <div className="WeeklyForecast">
       <AddressSearch onUpdateForecast={updateForecast} stations={stations}></AddressSearch>
+      <span className="WeeklyForecast__Error">{error}</span>
       {forecast.map((item: DayNightForecastDto, index) => (
         <DayNightForecast {...item}></DayNightForecast>
       ))}
